fd9986f4fb289846bea70e0f5b38713a3c50448e
enigmaticcam <enigmaticcam@hotmail.com>
2015-09-11 22:02:51 -0700

    Problem 42 finished

--- Euler_Win/frmMain.cs
+++ Euler_Win/frmMain.cs
@@ -57,6 +57,7 @@ namespace Euler_Win {
             _problems.Add(new Problem39());
             _problems.Add(new Problem40());
             _problems.Add(new Problem41());
+            _problems.Add(new Problem42());
             _problems.Add(new Problem44());
             _problems.Add(new Problem49());
             _problems.Add(new Problem55());
