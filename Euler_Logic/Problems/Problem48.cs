d6e7918194f17ce3b318f1f06bce7923f2ae909f
enigmaticcam <enigmaticcam@hotmail.com>
2015-09-17 14:13:21 -0700

    Problem 48 finished

---
+++ Euler_Logic/Problems/Problem48.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Logic.Problems {
+    public class Problem48 : IProblem {
+        
+
+        public string ProblemName {
+            get { return "48: Self powers"; }
+        }
+
+        public string GetAnswer() {
+            return StripCharacters(CalcAllSelfPowers(1000)).ToString();
+        }
+
+        public decimal CalcAllSelfPowers(decimal max) {
+            decimal sum = 0;
+            for (int index = 1; index <= max; index++) {
+                sum += CalcSelfPower(index);
+            }
+            return sum;
+        }
+
+        private decimal CalcSelfPower(decimal max) {
+            decimal num = max;
+            for (decimal index = 2; index <= max; index++) {
+                num *= max;
+                num = StripCharacters(num);
+            }
+            return num;
+        }
+
+        private decimal Multiply(decimal a, decimal b) {
+            decimal sum = a;
+            for (int count = 1; count < b; count++) {
+                sum += a;
+                sum = StripCharacters(sum);
+            }
+            return sum;
+        }
+
+        private decimal StripCharacters(decimal num) {
+            if (num < 10000000000) {
+                return num;
+            } else {
+                string text = num.ToString();
+                decimal power = 0;
+                for (int index = 1; index <= 10; index++) {
+                    power += Convert.ToDecimal(text.Substring(text.Length - index, 1)) * (decimal)Math.Pow(10, index - 1);
+                }
+                string powerAsString = power.ToString();
+                if (powerAsString.Length > 10) {
+                    return Convert.ToInt32(powerAsString.Substring(powerAsString.Length - 10, 10));
+                } else {
+                    return power;
+                }
+            }
+        }
+    }
+}
--- Euler_Win/frmMain.cs
+++ Euler_Win/frmMain.cs
@@ -62,6 +62,7 @@ namespace Euler_Win {
             _problems.Add(new Problem44());
             _problems.Add(new Problem45());
             _problems.Add(new Problem46());
+            _problems.Add(new Problem48());
             _problems.Add(new Problem49());
             _problems.Add(new Problem55());
             _problems.Add(new Problem58());
