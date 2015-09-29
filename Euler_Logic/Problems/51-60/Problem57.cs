5343269ec2c0d86a3cf2638aa0e7dc4029917445
enigmaticcam <enigmaticcam@hotmail.com>
2015-09-29 16:23:31 -0700

    Still workiong on Problem 57

--- Euler_Logic/Problems/51-60/Problem57.cs
+++ Euler_Logic/Problems/51-60/Problem57.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Euler_Logic.Problems {
     public class Problem57 : IProblem {
-        private double _lastDenominator = 2;
+        private ulong _lastDenominator = 2;
 
         public string ProblemName {
             get { return "57: Square root convergents"; }
@@ -17,50 +17,45 @@ namespace Euler_Logic.Problems {
         }
 
         private int GetTotalCount(int max) {
-            int count = 0;
+            int count = 1;
             int total = 0;
-            double expansion = 0.5;
-            double answer = 1.5;
-            double denominator = 2;
-            double numerator = 3;
+            ulong denominator = 2;
+            ulong numerator = 3;
+            ulong temp = 0;
             do {
-                if (count == 800) {
-                    bool stophere = true;
-                }
-                double reducedFactor = CanBeReduced(denominator, numerator);
-                if (reducedFactor > 0 && StripDecimal((numerator / reducedFactor).ToString()).Length > StripDecimal((denominator / reducedFactor).ToString()).Length) {
+                numerator += denominator;
+                temp = numerator;
+                numerator = denominator;
+                denominator = temp;
+                numerator += denominator;
+                if (numerator.ToString().Length > denominator.ToString().Length) {
                     total++;
-                } else if (StripDecimal(numerator.ToString()).Length > StripDecimal(denominator.ToString()).Length) {
-                    total++;
                 }
-                numerator = (answer + (double)2) * denominator;
-                expansion = NextExpansion(expansion);
-                answer = (double)1 + expansion;
-                denominator = numerator / answer;
-
                 count++;
-            } while (count <= max);
+            } while (count < max);
             return total;
         }
 
-        private string StripDecimal(string num) {
-            if (num.IndexOf(".") == -1) {
-                return num;
+        private int Stripulong(string num) {
+            if (num.Contains("E+")) {
+                return num.IndexOf(".") + Convert.ToInt32(num.Substring(num.IndexOf("E+") + 2, num.Length - num.IndexOf("E+") - 2));
+            } else if (num.Contains(".")) {
+                return num.Substring(0, num.IndexOf(".")).Length;
             } else {
-                return num.Substring(0, num.IndexOf("."));
+                return num.Length;
             }
         }
 
-        private double CanBeReduced(double denominator, double numerator) {
-            for (double factor = Math.Sqrt(denominator); factor >= 2; factor--) {
-                if (denominator % factor == 0 && numerator % factor == 0) {
-                    return factor;
-                }
-            }
-            return 0;
-        }
+        //private ulong CanBeReduced(ulong denominator, ulong numerator) {
+        //    for (ulong factor = Math.Sqrt(denominator); factor >= 2; factor--) {
+        //        if (denominator % factor == 0 && numerator % factor == 0) {
+        //            return factor;
+        //        }
+        //    }
+        //    return 0;
+        //}
 
-        private double NextExpansion(double num) {
+        private ulong NextExpansion(ulong num) {
             return 1 / (2 + num);
         }
 
