23e9e5f5b6822d290c0930ee98a1d432e3fb4301
enigmaticcam <enigmaticcam@hotmail.com>
2015-11-04 16:17:46 -0800

    Working on Problem 75

--- Euler_Logic/Problems/071-080/Problem75.cs
+++ Euler_Logic/Problems/071-080/Problem75.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace Euler_Logic.Problems {
     public class Problem75 : IProblem {
-        private Dictionary<double, double> _triangleCount = new Dictionary<double, double>();
         private HashSet<double> _squares = new HashSet<double>();
-        private List<double> _squaresList = new List<double>();
+        private List<double> _squaresList;
+        private Dictionary<double, int> _numbers = new Dictionary<double, int>();
+        private HashSet<double> _primes = new HashSet<double>();
 
         public string ProblemName {
             get { return "75: Singular Integer Right Triangles"; }
@@ -16,52 +17,92 @@ namespace Euler_Logic.Problems {
 
         public string GetAnswer() {
             double max = 1500000;
+            SievePrimes(max);
             GenerateSquares(max);
-            for (int i = 0; i < _squares.Count; i++) {
-                LookForIntegerTriangles(i, max);
-            }
-            int count = 0;
-            foreach (double value in _triangleCount.Keys) {
-                if (_triangleCount[value] == 1) {
-                    count++;
-                }
-            }
-            return count.ToString();
+            NewTest(max);
+            return GetFinalCount(max).ToString();
         }
 
         private void GenerateSquares(double max) {
-            double actualMax = max;
-            double a = 1;
-            do {
-                _squares.Add(a * a);
-                _squaresList.Add(a * a);
-                a++;
-            } while (a <= actualMax);
+            for (double num = 2; num <= max; num++) {
+                _squares.Add(num * num);
+            }
+            _squaresList = _squares.ToList();
         }
 
-        public void LookForIntegerTriangles(int start, double max) {
-            double a = _squaresList[start];
-            double b = 0;
-            double c = 0;
-            if (start < _squaresList.Count - 1) {
-                b = _squaresList[start];
-                c = a + b;
-                while (a + b >= _squaresList[start + 1] && Math.Sqrt(a) + Math.Sqrt(b) + Math.Sqrt(c) <= max && start < _squaresList.Count - 1) {
-                    if (_squares.Contains(c)) {
-                        double final = Math.Sqrt(a) + Math.Sqrt(b) + Math.Sqrt(c);
-                        if (!_triangleCount.ContainsKey(final)) {
-                            _triangleCount.Add(final, 1);
-                        } else {
-                            _triangleCount[final] += 1;
-                        }
-                    }
-                    start++;
-                    if (start < _squaresList.Count - 1) {
-                        b = _squaresList[start];
-                        c = a + b;
-                    }
+        private void NewTest(double max) {
+            _primes.Remove(2);
+            _primes.Remove(3);
+            foreach (double hypotenuse in _primes) {
+                if (!CheckIfHypotenuseIsGood(hypotenuse, max)) {
+                    break;
                 }
             }
         }
+
+        private bool CheckIfHypotenuseIsGood(double hypotenuse, double max) {
+            double hypSquared = hypotenuse * hypotenuse;
+            int squareIndex = 0;
+            bool isFirst = true;
+            do {
+                if (_squares.Contains(hypSquared - _squaresList[squareIndex])) {
+                    double a = Math.Sqrt(_squaresList[squareIndex]);
+                    double b = Math.Sqrt(hypSquared - _squaresList[squareIndex]);
+                    if (isFirst && a + b + hypotenuse > max) {
+                        return false;
+                    } else {
+                        isFirst = false;
+                    }
+                    SeiveTrianglePerimeter(a, b, hypotenuse, max);
+                    break;
+                }
+                squareIndex++;
+            } while (_squaresList[squareIndex] < hypSquared / 2);
+            return true;
+        }
+
+        private void SeiveTrianglePerimeter(double a, double b, double hypotenuse, double max) {
+            double compositeFactor = 1;
+            double length = 0;
+            do {
+                length = (a * compositeFactor) + (b * compositeFactor) + (hypotenuse * compositeFactor);
+                if (_numbers.ContainsKey(length)) {
+                    _numbers[length]++;
+                } else {
+                    _numbers.Add(length, 1);
+                }
+                compositeFactor++;
+            } while (length < max);
+        }
+
+        private int GetFinalCount(double max) {
+            int count = 0;
+            List<double> keys = _numbers.Keys.ToList();
+            keys.Sort();
+            foreach (double length in keys) {
+                if (length > max) {
+                    break;
+                }
+                if (_numbers[length] == 1) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private void SievePrimes(double max) {
+            HashSet<double> numbers = new HashSet<double>();
+            for (double num = 2; num <= max; num++) {
+                if (!numbers.Contains(num)) {
+                    _primes.Add(num);
+                    double composite = num;
+                    do {
+                        numbers.Add(composite);
+                        composite += num;
+                    } while (composite <= max);
+                }
+            }
+        }
+        
     }
 }
