000be83770894fdd456e0c58ee9f75ae15df6162
enigmaticcam <enigmaticcam@hotmail.com>
2015-11-04 16:28:09 -0800

    Fixed Problem 10. Was too slow!

--- Euler_Logic/Problems/001-010/Problem10.cs
+++ Euler_Logic/Problems/001-010/Problem10.cs
@@ -6,37 +6,29 @@ using System.Threading.Tasks;
 
 namespace Euler_Logic.Problems {
     public class Problem10 : IProblem {
+
         public string ProblemName {
             get { return "10: Summation of primes"; }
         }
 
         public string GetAnswer() {
-            return SumOfPrimes(2000000).ToString();
+            return SievePrimes(2000000).ToString();
         }
 
-        private decimal SumOfPrimes(decimal max) {
-            decimal sum = 2;
-            for (decimal i = 3; i < max; i += 2) {
-                if (IsPrime(i)) {
-                    sum += i;
+        private double SievePrimes(double max) {
+            double sum = 0;
+            HashSet<double> numbers = new HashSet<double>();
+            for (double num = 2; num <= max; num++) {
+                if (!numbers.Contains(num)) {
+                    sum += num;
+                    double composite = num;
+                    do {
+                        numbers.Add(composite);
+                        composite += num;
+                    } while (composite <= max);
                 }
             }
             return sum;
-        }
-
-        private bool IsPrime(decimal num) {
-            if (num == 2) {
-                return true;
-            } else if (num % 2 == 0) {
-                return false;
-            } else {
-                for (ulong factor = 3; factor <= Math.Sqrt((double)num); factor += 2) {
-                    if (num % factor == 0) {
-                        return false;
-                    }
-                }
-            }
-            return true;
         }
     }
 }
