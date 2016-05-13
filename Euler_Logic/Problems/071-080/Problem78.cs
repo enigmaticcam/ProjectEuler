1da864210346e9d58e67e3ec2a4a6b6b352a9ac7
enigmaticcam <enigmaticcam@hotmail.com>
2016-05-13 15:20:54 -0700

    Update on 78

--- Euler_Logic/Problems/071-080/Problem78.cs
+++ Euler_Logic/Problems/071-080/Problem78.cs
@@ -7,40 +7,40 @@ using System.Threading.Tasks;
 
 namespace Euler_Logic.Problems {
     public class Problem78 : IProblem {
-        private List<BigInteger> _primes = new List<BigInteger>();
-        private List<Dictionary<BigInteger, BigInteger>> _primeCounts = new List<Dictionary<BigInteger, BigInteger>>();
+        private List<int> _primes = new List<int>();
+        private List<Dictionary<int, int>> _primeCounts = new List<Dictionary<int, int>>();
 
         public string ProblemName {
             get { return "78: Coin Partitions"; }
         }
 
         public string GetAnswer() {
-            _primeCounts.Add(new Dictionary<BigInteger, BigInteger>());
+            _primeCounts.Add(new Dictionary<int, int>());
             return CountPiles().ToString();
         }
 
-       private BigInteger CountPiles() {
-            BigInteger num = 1;
+       private int CountPiles() {
+            int num = 1;
             do {
                 for (int primeIndex = 0; primeIndex < _primes.Count; primeIndex++) {
                     BuildSums(num, primeIndex, num);
                 }
                 _primes.Add(num);
-                _primeCounts.Add(new Dictionary<BigInteger, BigInteger>());
+                _primeCounts.Add(new Dictionary<int, int>());
                 BuildSums(1, _primes.Count - 1, num);
-                BigInteger count = GetCount(_primes.Count - 1, num);
-                if (count != 0 && count % 1000000 == 0) {
+                int count = GetCount(_primes.Count - 1, num);
+                if (count == 0) {
                     return num;
                 }
                 num++;
             } while (true);
         }
 
-       private void BuildSums(BigInteger weight, int primeIndex, BigInteger num) {
-           for (BigInteger weightIndex = weight; weightIndex <= num; weightIndex++) {
-               BigInteger tempWeight = 0;
+       private void BuildSums(int weight, int primeIndex, int num) {
+           for (int weightIndex = weight; weightIndex <= num; weightIndex++) {
+               int tempWeight = 0;
                 while (tempWeight <= weightIndex) {
-                    BigInteger count = GetCount(primeIndex, weightIndex);
+                    int count = GetCount(primeIndex, weightIndex);
                     if (tempWeight == weightIndex) {
                         SetCount(primeIndex, weightIndex, count + 1);
                     } else {
@@ -51,7 +51,7 @@ namespace Euler_Logic.Problems {
             }
         }
 
-       private BigInteger GetCount(int primeIndex, BigInteger weight) {
+       private int GetCount(int primeIndex, int weight) {
             primeIndex += 1;
             if (!_primeCounts[primeIndex].ContainsKey(weight)) {
                 _primeCounts[primeIndex].Add(weight, 0);
@@ -59,12 +59,12 @@ namespace Euler_Logic.Problems {
             return _primeCounts[primeIndex][weight];
         }
 
-       private void SetCount(int primeIndex, BigInteger weight, BigInteger value) {
+       private void SetCount(int primeIndex, int weight, int value) {
             primeIndex += 1;
             if (!_primeCounts[primeIndex].ContainsKey(weight)) {
-                _primeCounts[primeIndex].Add(weight, value);
+                _primeCounts[primeIndex].Add(weight, value % 1000000);
             } else {
-                _primeCounts[primeIndex][weight] = value;
+                _primeCounts[primeIndex][weight] = value % 1000000;
             }
         }
     }
