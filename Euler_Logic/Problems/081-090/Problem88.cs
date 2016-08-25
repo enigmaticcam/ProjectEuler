1d96dde4b45116be72d4f324cc047177e7854a33
enigmaticcam <enigmaticcam@hotmail.com>
2016-08-25 11:34:38 -0700

    Finished problem 88!

--- Euler_Logic/Problems/081-090/Problem88.cs
+++ Euler_Logic/Problems/081-090/Problem88.cs
@@ -6,59 +6,89 @@ using System.Threading.Tasks;
 
 namespace Euler_Logic.Problems {
     public class Problem88 : IProblem {
-        private int[] _numbers = new int[12000];
-        private Dictionary<int, Dictionary<int, Dictionary<int, bool>>> _hash = new Dictionary<int, Dictionary<int, Dictionary<int, bool>>>();
+        private int[] _factors;
+        private bool[] _notPrimes;
+        private int[] _sums;
 
         public string ProblemName {
             get { return "88: Product-sum numbers"; }
         }
 
         public string GetAnswer() {
-            return Solve(12000).ToString();
+            int max = 12000;
+            _factors = new int[max * 2];
+            _sums = new int[max * 2 + 1];
+            Initialize();
+            SievePrimes(max * 2);
+            Solve(max);
+            return GetSum(max).ToString();
         }
 
-        private int Solve(int max) {
-            HashSet<int> numsFound = new HashSet<int>();
+        private void Initialize() {
+            for (int index = 0; index <= _sums.GetUpperBound(0); index++) {
+                _sums[index] = int.MaxValue;
+            }
+        }
+
+        private int GetSum(int max) {
+            HashSet<int> sums = new HashSet<int>();
             int sum = 0;
-            for (int digits = 2; digits <= max; digits++) {
-                int k = digits;
-                do {
-                    k++;
-                } while (!FindLowest(digits, k, k));
-                if (!numsFound.Contains(k)) {
-                    sum += k;
-                    numsFound.Add(k);
+            for (int index = 0; index <= max; index++) {
+                if (_sums[index] != int.MaxValue && !sums.Contains(_sums[index])) {
+                    sum += _sums[index];
+                    sums.Add(_sums[index]);
                 }
             }
             return sum;
         }
 
-        private bool FindLowest(int digitsRemaining, int sum, int mult) {
-            for (int num = 1; num <= sum; num++) {
-                if (mult % num == 0) {
-                    int newSum = sum - num;
-                    int newMult = mult / num;
-                    if (digitsRemaining == 2) {
-                        if (newSum == newMult) {
-                            return true;
+        private void Solve(int max) {
+            int num = 4;
+            do {
+                BuildPrimeFactors(num, num, 0);
+                num++;
+            } while (num <= max * 2);
+        }
+
+        private void BuildPrimeFactors(int maxNum, int num, int factorCount) {
+            int max = num;
+            for (int prime = 2; prime < max; prime++) {
+                if (num % prime == 0) {
+                    max = num / prime;
+                    _factors[factorCount] = prime;
+                    if (num / prime != 1) {
+                        _factors[factorCount + 1] = num / prime;
+                        CheckProductSums(maxNum, factorCount + 1);
+                        if (_notPrimes[num / prime]) {
+                            BuildPrimeFactors(maxNum, num / prime, factorCount + 1);
                         }
                     } else {
-                        if (!_hash.ContainsKey(digitsRemaining - 1)) {
-                            _hash.Add(digitsRemaining - 1, new Dictionary<int, Dictionary<int, bool>>());
-                        }
-                        if (!_hash[digitsRemaining - 1].ContainsKey(newSum)) {
-                            _hash[digitsRemaining - 1].Add(newSum, new Dictionary<int, bool>());
-                        }
-                        if (!_hash[digitsRemaining - 1][newSum].ContainsKey(newMult)) {
-                            _hash[digitsRemaining - 1][newSum].Add(newMult, FindLowest(digitsRemaining - 1, newSum, newMult));
-                        }
-                        if (_hash[digitsRemaining - 1][newSum][newMult]) {
-                            return true;
-                        }
+                        CheckProductSums(maxNum, factorCount);
                     }
                 }
             }
-            return false;
+        }
+
+        private void CheckProductSums(int maxNum, int factorCount) {
+            int sum = 0;
+            for (int index = 0; index <= factorCount; index++) {
+                sum += _factors[index];
+            }
+            int digitCount = 1 + factorCount + (maxNum - sum);
+            if (_sums[digitCount] > maxNum) {
+                _sums[digitCount] = maxNum;
+            }
+        }
+
+        private void SievePrimes(int max) {
+            _notPrimes = new bool[max + 1];
+            for (int num = 2; num <= max; num++) {
+                if (!_notPrimes[num]) {
+                    for (int composite = 2; composite * num <= max; composite++) {
+                        _notPrimes[composite * num] = true;
+                    }
+                }
+            }
         }
     }
 }
