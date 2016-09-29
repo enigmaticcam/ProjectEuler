95d94669de20e3c23d68c7a54ee1960c5b75684b
enigmaticcam <enigmaticcam@hotmail.com>
2016-09-29 14:30:36 -0700

    Finished problem 173!

---
+++ Euler_Logic/Problems/171-180/Problem173.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Logic.Problems {
+    public class Problem173 : IProblem {
+        public string ProblemName {
+            get { return "173: Using up to one million tiles how many different 'hollow' square laminae can be formed?"; }
+        }
+
+        public string GetAnswer() {
+            return Solve(1000000).ToString();
+        }
+
+        public ulong Solve(ulong max) {
+            ulong sum = 0;
+            for (ulong num = 1; num <= max / 4; num++) {
+                ulong subSum = (num + 1) * 4;
+                ulong nextNum = subSum;
+                while (subSum <= max) {
+                    sum += 1;
+                    nextNum = ((nextNum / 4) + 2) * 4;
+                    subSum += nextNum;
+                }
+            }
+            return sum;
+        }
+    }
+}
--- Euler_Win/frmMain.cs
+++ Euler_Win/frmMain.cs
@@ -127,6 +127,7 @@ namespace Euler_Win {
             _problems.Add(new Problem124());
             _problems.Add(new Problem125());
             _problems.Add(new Problem145());
+            _problems.Add(new Problem173());
             _problems.Add(new Problem179());
             _problems.Add(new Problem191());
             _problems.Add(new Problem205());
