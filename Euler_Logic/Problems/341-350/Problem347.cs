841759f8467841ca55d372b944919b5a00b7ebf2
Cameron Tangen <enigmaticcam@hotmail.com>
2017-09-12 08:25:33 -0700

    Finished problem 347!

---
+++ Euler_Logic/Problems/341-350/Problem347.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Logic.Problems {
+    public class Problem347 : ProblemBase {
+        private bool[] _notPrimes;
+        private List<double> _primes = new List<double>();
+
+        public override string ProblemName {
+            get { return "347: Largest integer divisible by two primes"; }
+        }
+
+        public override string GetAnswer() {
+            double max = 10000000;
+            SievePrimes(max);
+            return Solve(max).ToString();
+        }
+
+        private void SievePrimes(double max) {
+            _notPrimes = new bool[(int)max + 1];
+            for (int num = 2; num <= max; num++) {
+                if (!_notPrimes[num]) {
+                    _primes.Add(num);
+                    for (int composite = 2; composite * num <= max; composite++) {
+                        _notPrimes[composite * num] = true;
+                    }
+                }
+            }
+            _notPrimes = null;
+        }
+
+        private double Solve(double max) {
+            double sum = 0;
+            for (int x = 0; x < _primes.Count; x++) {
+                for (int y = x + 1; y < _primes.Count; y++) {
+                    if (_primes[y] * _primes[x] <= max) {
+                        double xPower = 1;
+                        double xResult = Math.Pow(_primes[y], xPower);
+                        double highest = 0;
+                        while (xResult <= max) {
+                            double remaining = max / xResult;
+                            int nextPower = (int)Math.Log(remaining, _primes[x]);
+                            if (nextPower == 0) {
+                                break;
+                            }
+                            double yResult = Math.Pow(_primes[x], nextPower);
+                            double result = yResult * xResult;
+                            if (result > highest) {
+                                highest = result;
+                            }
+                            xPower++;
+                            xResult = Math.Pow(_primes[y], xPower);
+                        }
+                        sum += highest;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
--- Euler_Logic/Problems/541-550/Problem549.cs
+++ Euler_Logic/Problems/541-550/Problem549.cs
@@ -5,64 +5,99 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Euler_Logic.Problems {
+    // New Method
     public class Problem549 : ProblemBase {
         private bool[] _notPrimes;
+        private HashSet<uint> _primes = new HashSet<uint>();
 
         public override string ProblemName {
             get { return "549: Divisibility of factorials"; }
         }
 
         public override string GetAnswer() {
-            uint max = 10000000;
+            uint max = 100000000;
             SievePrimes(max);
-            return Solve(max).ToString();
+            PrimeFactor(max);
+            return "";
+            //return Solve(max).ToString();
         }
 
         private void SievePrimes(uint max) {
             _notPrimes = new bool[max + 1];
             for (uint num = 2; num <= max; num++) {
                 if (!_notPrimes[num]) {
+                    _primes.Add(num);
                     for (uint composite = 2; composite * num <= max; composite++) {
                         _notPrimes[composite * num] = true;
                     }
                 }
             }
+            _notPrimes = null;
         }
 
-        private uint Solve(uint max) {
-            uint sum = 0;
-            for (uint num = 2; num <= max; num++) {
-                sum += FindNextFactorial(num);
-            }
-            return sum;
-        }
-
-        private uint FindPowers(uint num, uint max) {
-            uint sum = num;
-            uint next = sum * 2;
-            uint basePower = 2;
-            uint power = (uint)Math.Pow(num, basePower);
-            while (power <= max) {
-                sum += FindNextFactorial(power);
-                basePower++;
-                power = (uint)Math.Pow(num, basePower);
-            }
-            return sum;
-        }
-
-        private uint FindNextFactorial(uint num) {
-            if (!_notPrimes[num]) {
-                return num;
-            }
-            uint baseNum = 3;
-            uint factorial = 2;
-            do {
-                factorial = (factorial * baseNum) % num;
-                if (factorial == 0) {
-                    return baseNum;
+        private void PrimeFactor(uint max) {
+            for (uint num = 4; num <= max; num++) {
+                if (!_primes.Contains(num)) {
+                    uint leftovers = num;
+                    foreach (uint prime in _primes) {
+                        while (leftovers % prime == 0) {
+                            leftovers /= prime;
+                        }
+                        if (leftovers == 1 || _primes.Contains(leftovers)) {
+                            break;
+                        }
+                    }
                 }
-                baseNum++;
-            } while (true);
+            }
         }
     }
+
+    // Old Method
+    //public class Problem549 : ProblemBase {
+    //    private bool[] _notPrimes;
+
+    //    public override string ProblemName {
+    //        get { return "549: Divisibility of factorials"; }
+    //    }
+
+    //    public override string GetAnswer() {
+    //        uint max = 100;
+    //        SievePrimes(max);
+    //        return Solve(max).ToString();
+    //    }
+
+    //    private void SievePrimes(uint max) {
+    //        _notPrimes = new bool[max + 1];
+    //        for (uint num = 2; num <= max; num++) {
+    //            if (!_notPrimes[num]) {
+    //                for (uint composite = 2; composite * num <= max; composite++) {
+    //                    _notPrimes[composite * num] = true;
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //    private uint Solve(uint max) {
+    //        uint sum = 0;
+    //        for (uint num = 2; num <= max; num++) {
+    //            sum += FindNextFactorial(num);
+    //        }
+    //        return sum;
+    //    }
+
+    //    private uint FindNextFactorial(uint num) {
+    //        if (!_notPrimes[num]) {
+    //            return num;
+    //        }
+    //        uint baseNum = 3;
+    //        uint factorial = 2;
+    //        do {
+    //            factorial = (factorial * baseNum) % num;
+    //            if (factorial == 0) {
+    //                return baseNum;
+    //            }
+    //            baseNum++;
+    //        } while (true);
+    //    }
+    //}
 }
