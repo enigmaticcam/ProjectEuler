fb7dd6bcd4e6046bcb1c55720225670fdad44f91
Cameron Tangen <ctangen81@gmail.com>
2018-07-16 20:13:56 -0700

    Solved problem 323!

--- Euler_Logic/Problems/321-329/Problem323.cs
+++ Euler_Logic/Problems/321-329/Problem323.cs
@@ -1,50 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Euler_Logic.Problems {
     public class Problem323 : ProblemBase {
         private Random _random = new Random();
+        private Dictionary<double, Dictionary<double, double>> _combinations = new Dictionary<double, Dictionary<double, double>>();
+        private Dictionary<double, double> _factorials = new Dictionary<double, double>();
+        private Dictionary<int, Dictionary<int, double>> _subsets = new Dictionary<int, Dictionary<int, double>>();
+        private Dictionary<int, Dictionary<int, double>> _increments = new Dictionary<int, Dictionary<int, double>>();
+        private Dictionary<int, double> _expectedValues = new Dictionary<int, double>();
 
         public override string ProblemName {
             get { return "323: Bitwise-OR operations on random integers"; }
         }
 
         public override string GetAnswer() {
-            return Solve().ToString();
+            //return Test(16).ToString();
+            int maxDigits = 33;
+            CalcSubsets(maxDigits);
+            CalcIncrements(maxDigits);
+            CalcExpectedValues(maxDigits);
+            return _expectedValues[maxDigits - 1].ToString();
         }
 
-        private decimal Solve() {
-            decimal totalCount = 10000000;
-            decimal sum = 0;
-            for (int count = 1; count <= totalCount; count++) {
-                sum += PerformRandom();
+        private double Test(int maxDigits) {
+            double sum = 0;
+            double maxCount = 10000000;
+            int maxValue = (int)Math.Pow(2, maxDigits) - 1;
+            for (int index = 1; index <= maxCount; index++) {
+                double count = 0;
+                int last = 0;
+                do {
+                    count++;
+                    int number = _random.Next(0, maxValue + 1);
+                    last = number | last;
+                    if (last == maxValue) {
+                        sum += count;
+                        break;
+                    }
+                } while (true);
             }
-            return sum / totalCount;
+            return sum / maxCount;
         }
 
-        private decimal PerformRandom() {
-            decimal count = 0;
-            int last = 99;
-            do {
-                count++;
-                //uint thirtyBits = (uint)_random.Next(1 << 30);
-                //uint twoBits = (uint)_random.Next(1 << 2);
-                //uint num = (thirtyBits << 2) | twoBits;
-                //last = num | last;
-                //if (last == uint.MaxValue) {
-                //    return count;
-                //}
-                int num = _random.Next(1, 6);
-                if (num == 1) {
-                    return count;
-                } else if (num > last) {
-                    return count;
+        private void CalcSubsets(int maxDigits) {
+            for (int n = 1; n <= maxDigits; n++) {
+                _subsets.Add(n, new Dictionary<int, double>());
+                _subsets[n].Add(0, 1);
+                for (int r = 1; r <= n; r++) {
+                    double result = DistinctCombinationsOfSetInSet(n, r);
+                    _subsets[n].Add(r, result);
                 }
-                last = num;
-            } while (true);
+            }
+        }
+
+        private void CalcIncrements(int maxDigits) {
+            double max = Math.Pow(2, maxDigits);
+            for (int maxZeroes = 1; maxZeroes <= maxDigits; maxZeroes++) {
+                _increments.Add(maxZeroes, new Dictionary<int, double>());
+                for (int zeroCount = 0; zeroCount <= maxZeroes; zeroCount++) {
+                    _increments[maxZeroes].Add(zeroCount, _subsets[maxZeroes][zeroCount] * Math.Pow(2, maxDigits - maxZeroes) / max);
+                }
+            }
+        }
+
+        public void CalcExpectedValues(int maxDigits) {
+            _expectedValues.Add(0, 1);
+            for (int maxZeroes = 1; maxZeroes < maxDigits; maxZeroes++) {
+                double value = _increments[maxZeroes][0];
+                for (int zeroCount = 1; zeroCount < maxZeroes; zeroCount++) {
+                    value += ((1 + _expectedValues[zeroCount]) * _increments[maxZeroes][zeroCount]);
+                }
+                value += _increments[maxZeroes][maxZeroes];
+                value = value / (1 - _increments[maxZeroes][maxZeroes]);
+                _expectedValues.Add(maxZeroes, value);
+            }
+        }
+
+        private double Factorial(double max) {
+            if (!_factorials.ContainsKey(max)) {
+                double sum = max;
+                for (double num = max - 1; num >= 1; num--) {
+                    sum *= num;
+                }
+                _factorials.Add(max, sum);
+            }
+            return _factorials[max];
+        }
+
+        private double DistinctCombinationsOfSetInSet(double n, double r) {
+            if (!_combinations.ContainsKey(n)) {
+                _combinations.Add(n, new Dictionary<double, double>());
+            }
+            if (!_combinations[n].ContainsKey(r)) {
+                if (n == r) {
+                    _combinations[n].Add(r, 1);
+                } else {
+                    _combinations[n].Add(r, Factorial(n) / (Factorial(r) * Factorial(n - r)));
+                }
+            }
+            return _combinations[n][r];
         }
     }
 }
