3d3ada615b6922a286c57765ee9d6a0bc0067f57
Cameron Tangen <Cameron.Tangen@tweglobal.com>
2021-12-10 16:36:12 -0800

    Started Advent of Code 2019 problems 22, 23.

---
+++ Euler_Logic/Problems/AdventOfCode/Y2019/Problem22.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Logic.Problems.AdventOfCode.Y2019 {
+    public class Problem22 : AdventOfCodeBase {
+        private List<Instruction> _instructions;
+
+        public enum enumInstructionType {
+            DealNewStack,
+            CutCards,
+            DealWithIncrement
+        }
+
+        public override string ProblemName {
+            get { return "Advent of Code 2019: 22"; }
+        }
+
+        public override string GetAnswer() {
+            //GetInstructions(Input_Test(3));
+            //return Answer2(10).ToString();
+            GetInstructions(Input_Test(5));
+            return Answer2(119315717514047).ToString();
+        }
+
+        private int Answer1(int maxValue) {
+            var cards = Enumerable.Range(0, maxValue).ToArray();
+            foreach (var instruction in _instructions) {
+                switch (instruction.InstructionType) {
+                    case enumInstructionType.CutCards:
+                        cards = CutCards(cards, (int)instruction.Value);
+                        break;
+                    case enumInstructionType.DealNewStack:
+                        cards = DealNewStack(cards);
+                        break;
+                    case enumInstructionType.DealWithIncrement:
+                        cards = DealWithIncrement(cards, (int)instruction.Value);
+                        break;
+                }
+            }
+            return IndexOf(2019, cards);
+        }
+
+        private long Answer2(long maxValue) {
+            long zeroIndex = 0;
+            long count = 0;
+            do {
+                zeroIndex = FindResultIndex(maxValue, zeroIndex);
+                count++;
+            } while (zeroIndex != 0);
+            return count;
+        }
+
+        private long FindResultIndex(long maxValue, long index) {
+            foreach (var instruction in _instructions) {
+                switch (instruction.InstructionType) {
+                    case enumInstructionType.CutCards:
+                        index -= instruction.Value;
+                        if (index < 0) {
+                            index = maxValue + index;
+                        } else {
+                            index %= maxValue;
+                        }
+                        break;
+                    case enumInstructionType.DealNewStack:
+                        index = maxValue - index - 1;
+                        break;
+                    case enumInstructionType.DealWithIncrement:
+                        index = (index * instruction.Value) % maxValue;
+                        break;
+                }
+            }
+            return index;
+        }
+
+        private int IndexOf(int num, int[] cards) {
+            for (int index = 0; index < cards.Length; index++) {
+                if (cards[index] == num) {
+                    return index;
+                }
+            }
+            return 0;
+        }
+
+        private int[] DealWithIncrement(int[] cards, int value) {
+            var newCards = new int[cards.Length];
+            int index = 0;
+            int newIndex = 0;
+            for (int count = 1; count <= cards.Length; count++) {
+                newCards[index] = cards[newIndex];
+                index = (index + value) % cards.Length;
+                newIndex++;
+            }
+            return newCards;
+        }
+
+        private int[] DealNewStack(int[] cards) {
+            var newCards = new int[cards.Length];
+            for (int index = 0; index < cards.Length; index++) {
+                newCards[index] = cards[cards.Length - index - 1];
+            }
+            return newCards;
+        }
+
+        private int[] CutCards(int[] cards, int value) {
+            var newCards = new int[cards.Length];
+            int count = 0;
+            int start = value;
+            if (value < 0) {
+                start = cards.Length + value;
+            }
+            for (int index = start; index < cards.Length; index++) {
+                newCards[count] = cards[index];
+                count++;
+            }
+            for (int index = 0; index < start; index++) {
+                newCards[count] = cards[index];
+                count++;
+            }
+            return newCards;
+        }
+
+        private void GetInstructions(List<string> input) {
+            _instructions = input.Select(x => {
+                var split = x.Split(' ');
+                var instruction = new Instruction();
+                if (split[0] == "cut") {
+                    instruction.InstructionType = enumInstructionType.CutCards;
+                    instruction.Value = Convert.ToInt32(split[1]);
+                } else if (split[1] == "with") {
+                    instruction.InstructionType = enumInstructionType.DealWithIncrement;
+                    instruction.Value = Convert.ToInt32(split[3]);
+                } else {
+                    instruction.InstructionType = enumInstructionType.DealNewStack;
+                }
+                return instruction;
+            }).ToList();
+        }
+
+        private class Instruction {
+            public enumInstructionType InstructionType { get; set; }
+            public long Value { get; set; }
+        }
+    }
+}
---
+++ Euler_Logic/Problems/AdventOfCode/Y2019/Problem23.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Logic.Problems.AdventOfCode.Y2019 {
+    public class Problem23 : AdventOfCodeBase {
+        private Dictionary<int, Computer> _computers;
+        private bool _sentTo255;
+        private int _valueSentTo255;
+
+        public override string ProblemName {
+            get { return "Advent of Code 2019: 23"; }
+        }
+
+        public override string GetAnswer() {
+            return Answer1().ToString();
+        }
+
+        private int Answer1() {
+            BootComputers();
+            LookForFirstTo255();
+            return _valueSentTo255;
+        }
+
+        private void BootComputers() {
+            _computers = new Dictionary<int, Computer>();
+            for (int address = 0; address < 50; address++) {
+                _computers.Add(address, new Computer() {
+                    Address = address,
+                    Comp = new IntComputer(),
+                    IsBooting = true,
+                    Packets = new LinkedList<Packet>()
+                });
+                _computers[address].Comp.SingleOutputOnly = true;
+            }
+        }
+
+        private void LookForFirstTo255() {
+            do {
+                foreach (var comp in _computers.Values) {
+                    comp.Comp.Run(Input(), () => InputCaller(comp), () => OutputCaller(comp));
+                }
+            } while (!_sentTo255);
+        }
+
+        private int InputCaller(Computer computer) {
+            if (computer.IsBooting) {
+                computer.IsBooting = false;
+                return computer.Address;
+            } else if (computer.Packets.Count == 0) {
+                return -1;
+            } else {
+                var packet = computer.Packets.First.Value;
+                if (!packet.IsXRead) {
+                    packet.IsXRead = true;
+                    return packet.X;
+                } else {
+                    computer.Packets.RemoveFirst();
+                    return packet.Y;
+                }
+            }
+        }
+
+        private void OutputCaller(Computer computer) {
+            if (computer.PacketPreparingToSend == null) {
+                computer.PacketPreparingToSend = new Packet() { Address = (int)computer.Comp.LastOutput };
+                computer.IsNextOtuputX = true;
+            } else if (computer.IsNextOtuputX) {
+                computer.PacketPreparingToSend.X = (int)computer.Comp.LastOutput;
+                computer.IsNextOtuputX = false;
+            } else {
+                computer.PacketPreparingToSend.Y = (int)computer.Comp.LastOutput;
+                if (computer.PacketPreparingToSend.Address == 255) {
+                    _valueSentTo255 = (int)computer.Comp.LastOutput;
+                    _sentTo255 = true;
+                } else {
+                    _computers[computer.PacketPreparingToSend.Address].Packets.AddLast(computer.PacketPreparingToSend);
+                    computer.PacketPreparingToSend = null;
+                }
+            }
+        }
+
+        private class Computer {
+            public int Address { get; set; }
+            public IntComputer Comp { get; set; }
+            public LinkedList<Packet> Packets { get; set; }
+            public bool IsBooting { get; set; }
+            public Packet PacketPreparingToSend { get; set; }
+            public bool IsNextOtuputX { get; set; }
+        }
+
+        private class Packet {
+            public int Address { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public bool IsXRead { get; set; }
+        }
+    }
+}
