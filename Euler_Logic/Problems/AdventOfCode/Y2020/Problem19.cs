fea5bc9644850d2ba2b7993d5d296b71ad5dcd86
Cameron Tangen <Cameron.Tangen@tweglobal.com>
2022-01-07 22:36:21 -0800

    Solved Advent of Code 2020 Day 19 part 2.

--- Euler_Logic/Problems/AdventOfCode/Y2020/Problem19.cs
+++ Euler_Logic/Problems/AdventOfCode/Y2020/Problem19.cs
@@ -1,265 +1,198 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Euler_Logic.Problems.AdventOfCode.Y2020 {
     public class Problem19 : AdventOfCodeBase {
-        private Dictionary<string, Rule> _rules;
-        private Dictionary<string, string> _overrides;
+        private Dictionary<int, Rule> _rules;
+        private List<List<Rule>> _tiers;
         private List<string> _messages;
+        private Dictionary<int, HashSet<string>> _valids;
+        private int _maxLength;
 
         public override string ProblemName => "Advent of Code 2020: 19";
 
         public override string GetAnswer() {
-            _overrides = new Dictionary<string, string>();
-            return Answer2(Test3Input()).ToString();
+            return Answer1(Input()).ToString();
+        }
+
+        public override string GetAnswer2() {
+            return Answer2(Input()).ToString();
         }
 
         private int Answer1(List<string> input) {
-            BuildRulesAndMessgaes(input);
-            return CoundValidRules();
+            GetRulesAndMessages(input);
+            SetMaxLength();
+            SetTiers();
+            BuildValids();
+            return FinalCount();
         }
 
         private int Answer2(List<string> input) {
-            _overrides.Add("8", "8: 42 | 42 8");
-            _overrides.Add("11", "11: 42 31 | 42 11 31");
-            BuildRulesAndMessgaes(input);
-            return CoundValidRules();
+            GetRulesAndMessages(input);
+            SetMaxLength();
+            SetTiers();
+            BuildValids();
+            return AddRecursionRules();
         }
 
-        private int CoundValidRules() {
+        private int AddRecursionRules() {
             int count = 0;
             foreach (var message in _messages) {
-                if (message == "babbbbaabbbbbabbbbbbaabaaabaaa") {
-                    bool stop = true;
-                }
-                var result = _rules["0"].DoesMatch(message, 0);
-                if (result.DidMatch && result.LastIndex == message.Length - 1) {
-                    count++;
-                }
+                if (RecursionRule8("", message)) count++;
             }
             return count;
         }
 
-        private void BuildRulesAndMessgaes(List<string> input) {
-            _rules = new Dictionary<string, Rule>();
-            int lineIndex = 0;
-            foreach (var line in input) {
+        private bool RecursionRule8(string current, string message) {
+            foreach (var message1 in _valids[42]) {
+                var temp = current + message1;
+                if (message.IndexOf(temp) == 0) {
+                    var result = Recursion11("", message.Substring(temp.Length));
+                    if (result) return true;
+                    result = RecursionRule8(temp, message);
+                    if (result) return true;
+                }
+            }
+            return false;
+        }
+
+        private bool Recursion11(string current, string message) {
+            foreach (var message1 in _valids[42]) {
+                var temp1 = message1 + current;
+                if (message.IndexOf(temp1) >= 0) {
+                    foreach (var message2 in _valids[31]) {
+                        var temp2 = temp1 + message2;
+                        if (message == temp2) {
+                            return true;
+                        } else if (message.IndexOf(temp2) >= 0) {
+                            var result = Recursion11(temp2, message);
+                            if (result) return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private int FinalCount() {
+            int count = 0;
+            var rule0 = _valids[0];
+            foreach (var message in _messages) {
+                if (rule0.Contains(message)) count++;
+            }
+            return count;
+        }
+
+        private void BuildValids() {
+            _valids = new Dictionary<int, HashSet<string>>();
+            _valids.Add(_tiers[0][0].Id, new HashSet<string>());
+            _valids.Add(_tiers[0][1].Id, new HashSet<string>());
+            _valids[_tiers[0][0].Id].Add(_tiers[0][0].Value);
+            _valids[_tiers[0][1].Id].Add(_tiers[0][1].Value);
+            for (int tier = 1; tier < _tiers.Count; tier++) {
+                foreach (var rule in _tiers[tier]) {
+                    var hash = new HashSet<string>();
+                    foreach (var text in BuildValids(rule.Side1)) {
+                        hash.Add(text);
+                    }
+                    if (rule.Side2.Count > 0) {
+                        foreach (var text in BuildValids(rule.Side2)) {
+                            hash.Add(text);
+                        }
+                    }
+                    _valids.Add(rule.Id, hash);
+                }
+            }
+        }
+
+        private IEnumerable<string> BuildValids(List<int> side) {
+            if (side.Count == 1) {
+                return new List<string>(_valids[side[0]]);
+            } else {
+                var join = 
+                    from a in _valids[side[0]]
+                    from b in _valids[side[1]]
+                    select a + b;
+                return join;
+            }
+        }
+
+        private void SetTiers() {
+            _tiers = new List<List<Rule>>();
+            var rules = new List<Rule>(_rules.Values);
+            var hash = new Dictionary<int, int>();
+            List<Rule> currentTier = null;
+            do {
+                currentTier = new List<Rule>();
+                for (int index = 0; index < rules.Count; index++) {
+                    var rule = rules[index];
+                    bool toAdd = true;
+                    if (!rule.IsValue) {
+                        foreach (var prior in rule.Side1.Union(rule.Side2)) {
+                            if (!hash.ContainsKey(prior) || hash[prior] == _tiers.Count) {
+                                toAdd = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (toAdd) {
+                        currentTier.Add(rule);
+                        hash.Add(rule.Id, _tiers.Count);
+                        rules.RemoveAt(index);
+                        index--;
+                    }
+                }
+                _tiers.Add(currentTier);
+            } while (rules.Count > 0);
+        }
+
+        private void SetMaxLength() {
+            _maxLength = _messages.Select(x => x.Length).Max();
+        }
+
+        private void GetRulesAndMessages(List<string> input) {
+            _rules = new Dictionary<int, Rule>();
+            _messages = new List<string>();
+            string line = "";
+            int index = 0;
+            do {
+                line = input[index];
                 if (line == "") {
                     break;
                 }
-                var rule = new Rule();
+                var rule = new Rule() { Side1 = new List<int>(), Side2 = new List<int>() };
                 var split = line.Split(' ');
-                if (_overrides.ContainsKey(split[0].Replace(":", ""))) {
-                    split = _overrides[split[0].Replace(":", "")].Split(' ');
-                }
-                if (split.Length == 2) {
-                    if (split[1][0] == '"') {
-                        rule.DoesMatch = (text, index) => new RuleResult() { DidMatch = index < text.Length && text[index] == split[1][1], LastIndex = index };
+                rule.Id = Convert.ToInt32(split[0].Replace(":", ""));
+                var side = rule.Side1;
+                for (int s = 1; s < split.Length; s++) {
+                    if (split[s] == "|") {
+                        side = rule.Side2;
+                    } else if (split[s] == "\"a\"") {
+                        rule.IsValue = true;
+                        rule.Value = "a";
+                    } else if (split[s] == "\"b\"") {
+                        rule.IsValue = true;
+                        rule.Value = "b";
                     } else {
-                        rule.DoesMatch = (text, index) => _rules[split[1]].DoesMatch(text, index);
-                    }
-                } else if (split.Length == 3) {
-                    rule.DoesMatch = (text, index) => {
-                        var a = _rules[split[1]].DoesMatch(text, index);
-                        if (a.DidMatch) {
-                            var b = _rules[split[2]].DoesMatch(text, a.LastIndex + 1);
-                            return b;
-                        } else {
-                            return new RuleResult() { DidMatch = false };
-                        }
-                    };
-                } else {
-                    bool hasPipe = false;
-                    for (int index = 1; index < split.Length; index++) {
-                        if (split[index] == "|") {
-                            hasPipe = true;
-                            break;
-                        }
-                    }
-                    if (!hasPipe) {
-                        if (split.Length == 4) {
-                            rule.DoesMatch = (text, index) => {
-                                var a = _rules[split[1]].DoesMatch(text, index);
-                                if (a.DidMatch) {
-                                    var b = _rules[split[2]].DoesMatch(text, a.LastIndex + 1);
-                                    if (b.DidMatch) {
-                                        return _rules[split[3]].DoesMatch(text, b.LastIndex + 1);
-                                    }
-                                }
-                                return new RuleResult() { DidMatch = false };
-                            };
-                        } else {
-                            throw new Exception();
-                        }
-                    } else if (split.Length == 4) {
-                        rule.DoesMatch = (text, index) => {
-                            var a = _rules[split[1]].DoesMatch(text, index);
-                            if (a.DidMatch) {
-                                return a;
-                            }
-                            var b = _rules[split[3]].DoesMatch(text, index);
-                            if (b.DidMatch) {
-                                return b;
-                            }
-                            return new RuleResult() { DidMatch = false };
-                        };
-                    } else if (split.Length == 5) {
-                        rule.DoesMatch = (text, index) => {
-                            var a = _rules[split[1]].DoesMatch(text, index);
-                            if (a.DidMatch) {
-                                return a;
-                            }
-                            //a = _rules[split[3]].DoesMatch(text, index);
-                            //if (a.DidMatch) {
-                            //    var b = _rules[split[4]].DoesMatch(text, a.LastIndex + 1);
-                            //    if (b.DidMatch) {
-                            //        return b;
-                            //    }
-                            //}
-                            return new RuleResult() { DidMatch = false };
-                        };
-                    } else if (split.Length == 6) {
-                        rule.DoesMatch = (text, index) => {
-                            var a = _rules[split[1]].DoesMatch(text, index);
-                            if (a.DidMatch) {
-                                var b = _rules[split[2]].DoesMatch(text, a.LastIndex + 1);
-                                if (b.DidMatch) {
-                                    return b;
-                                }
-                            }
-                            a = _rules[split[4]].DoesMatch(text, index);
-                            if (a.DidMatch) {
-                                var b = _rules[split[5]].DoesMatch(text, a.LastIndex + 1);
-                                if (b.DidMatch) {
-                                    return b;
-                                }
-                            }
-                            return new RuleResult() { DidMatch = false };
-                        };
-                    } else if (split.Length == 7) {
-                        rule.DoesMatch = (text, index) => {
-                            var a = _rules[split[1]].DoesMatch(text, index);
-                            if (a.DidMatch) {
-                                var b = _rules[split[2]].DoesMatch(text, a.LastIndex + 1);
-                                if (b.DidMatch) {
-                                    return b;
-                                }
-                            } else {
-                                return new RuleResult() { DidMatch = false };
-                            }
-                            a = _rules[split[4]].DoesMatch(text, index);
-                            if (a.DidMatch) {
-                                var b = _rules[split[5]].DoesMatch(text, a.LastIndex + 1);
-                                if (b.DidMatch) {
-                                    var c = _rules[split[6]].DoesMatch(text, b.LastIndex + 1);
-                                    if (c.DidMatch) {
-                                        return c;
-                                    }
-                                }
-                            }
-                            return new RuleResult() { DidMatch = false };
-                        };
-                    } else {
-                        throw new Exception();
+                        side.Add(Convert.ToInt32(split[s]));
                     }
                 }
-                _rules.Add(split[0].Replace(":", ""), rule);
-                lineIndex++;
+                _rules.Add(rule.Id, rule);
+                index++;
+            } while (true);
+            index++;
+            for (; index < input.Count; index++) {
+                _messages.Add(input[index]);
             }
-            _messages = input.Skip(lineIndex + 1).ToList();
-        }
-
-        private List<string> Test1Input() {
-            return new List<string>() {
-                "0: 1 2",
-                "1: \"a\"",
-                "2: 1 3 | 3 1",
-                "3: \"b\"",
-                "",
-                "aab",
-                "aba"
-            };
-        }
-
-        private List<string> Test2Input() {
-            return new List<string>() {
-                "0: 4 1 5",
-                "1: 2 3 | 3 2",
-                "2: 4 4 | 5 5",
-                "3: 4 5 | 5 4",
-                "4: \"a\"",
-                "5: \"b\"",
-                "",
-                "ababbb",
-                "bababa",
-                "abbbab",
-                "aaabbb",
-                "aaaabbb"
-            };
-        }
-
-        private List<string> Test3Input() {
-            return new List<string>() {
-                "42: 9 14 | 10 1",
-                "9: 14 27 | 1 26",
-                "10: 23 14 | 28 1",
-                "1: \"a\"",
-                "11: 42 31",
-                "5: 1 14 | 15 1",
-                "19: 14 1 | 14 14",
-                "12: 24 14 | 19 1",
-                "16: 15 1 | 14 14",
-                "31: 14 17 | 1 13",
-                "6: 14 14 | 1 14",
-                "2: 1 24 | 14 4",
-                "0: 8 11",
-                "13: 14 3 | 1 12",
-                "15: 1 | 14",
-                "17: 14 2 | 1 7",
-                "23: 25 1 | 22 14",
-                "28: 16 1",
-                "4: 1 1",
-                "20: 14 14 | 1 15",
-                "3: 5 14 | 16 1",
-                "27: 1 6 | 14 18",
-                "14: \"b\"",
-                "21: 14 1 | 1 14",
-                "25: 1 1 | 1 14",
-                "22: 14 14",
-                "8: 42",
-                "26: 14 22 | 1 20",
-                "18: 15 15",
-                "7: 14 5 | 1 21",
-                "24: 14 1",
-                "",
-                "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
-                "bbabbbbaabaabba",
-                "babbbbaabbbbbabbbbbbaabaaabaaa",
-                "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
-                "bbbbbbbaaaabbbbaaabbabaaa",
-                "bbbababbbbaaaaaaaabbababaaababaabab",
-                "ababaaaaaabaaab",
-                "ababaaaaabbbaba",
-                "baabbaaaabbaaaababbaababb",
-                "abbbbabbbbaaaababbbbbbaaaababb",
-                "aaaaabbaabaaaaababaa",
-                "aaaabbaaaabbaaa",
-                "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
-                "babaaabbbaaabaababbaabababaaab",
-                "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
-            };
         }
 
         private class Rule {
-            public Func<string, int, RuleResult> DoesMatch { get; set; }
-        }
-
-        private class RuleResult {
-            public bool DidMatch { get; set; }
-            public int LastIndex { get; set; }
+            public int Id { get; set; }
+            public List<int> Side1 { get; set; }
+            public List<int> Side2 { get; set; }
+            public string Value { get; set; }
+            public bool IsValue { get; set; }
         }
     }
 }
