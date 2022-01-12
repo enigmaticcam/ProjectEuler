9d48dcc07862e6e7d4c4d12316bb3c82ddd12cbe
Cameron Tangen <Cameron.Tangen@tweglobal.com>
2022-01-11 21:56:41 -0800

    Solved Advent of Code 2019 Day 22 part 2.

--- Euler_Logic/Helpers/Power.cs
+++ Euler_Logic/Helpers/Power.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Euler_Logic.Helpers {
     public class Power {
@@ -41,6 +42,19 @@ namespace Euler_Logic.Helpers {
 
         // Calculate x^y % z
         public static long Exp(long num, long exponent, long mod) {
+            if (exponent == 0) {
+                return 1;
+            } else if (exponent == 1) {
+                return num % mod;
+            } else if (exponent % 2 == 0) {
+                return Exp((num * num) % mod, exponent / 2, mod);
+            } else {
+                return (num * Exp((num * num) % mod, (exponent - 1) / 2, mod)) % mod;
+            }
+        }
+
+        // Calculate x^y % z
+        public static BigInteger Exp(BigInteger num, BigInteger exponent, BigInteger mod) {
             if (exponent == 0) {
                 return 1;
             } else if (exponent == 1) {
--- Euler_Logic/Problems/AdventOfCode/Y2019/Problem22.cs
+++ Euler_Logic/Problems/AdventOfCode/Y2019/Problem22.cs
@@ -2,12 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Euler_Logic.Problems.AdventOfCode.Y2019 {
     public class Problem22 : AdventOfCodeBase {
         private List<Instruction> _instructions;
+        private BigInteger _a;
+        private BigInteger _b;
 
         public enum enumInstructionType {
             DealNewStack,
@@ -20,123 +21,51 @@ namespace Euler_Logic.Problems.AdventOfCode.Y2019 {
         }
 
         public override string GetAnswer() {
-            //GetInstructions(Input_Test(3));
-            //return Answer2(10).ToString();
             GetInstructions(Input());
-            return Answer2(119315717514047).ToString();
+            return Answer1(10007, 2019).ToString();
         }
 
-        private int Answer1(int maxValue) {
-            var cards = Enumerable.Range(0, maxValue).ToArray();
+        public override string GetAnswer2() {
+            GetInstructions(Input());
+            return Answer2(119315717514047, 101741582076661).ToString();
+        }
+
+        private BigInteger Answer1(BigInteger maxValue, BigInteger position) {
+            SolveAB(maxValue);
+            return (_a * position + _b) % maxValue;
+        }
+
+        private void SolveAB(BigInteger maxValue) {
+            _a = 1;
+            _b = 0;
             foreach (var instruction in _instructions) {
                 switch (instruction.InstructionType) {
                     case enumInstructionType.CutCards:
-                        cards = CutCards(cards, (int)instruction.Value);
-                        break;
-                    case enumInstructionType.DealNewStack:
-                        cards = DealNewStack(cards);
-                        break;
-                    case enumInstructionType.DealWithIncrement:
-                        cards = DealWithIncrement(cards, (int)instruction.Value);
-                        break;
-                }
-            }
-            return IndexOf(2019, cards);
-        }
-
-        private long Answer2(long maxValue) {
-            Coalesce(maxValue);
-            return 0;
-        }
-
-        private long _a;
-        private long _b;
-        private void Coalesce(long maxValue) {
-            _a = 1;
-            for (int index = _instructions.Count - 1; index >= 0; index--) {
-                var instruction = _instructions[index];
-                switch (instruction.InstructionType) {
-                    case enumInstructionType.CutCards:
-                        _b = (_b + instruction.Value) % maxValue;
+                        _b -= instruction.Value;
+                        if (_b < 0) _b += maxValue;
                         break;
                     case enumInstructionType.DealNewStack:
                         _a *= -1;
-                        _b = maxValue - _b - 1;
+                        _b *= -1;
+                        _a += maxValue;
+                        _b += maxValue - 1;
+                        if (_b < 0) _b += maxValue;
                         break;
                     case enumInstructionType.DealWithIncrement:
-                        var z = Power.Exp(instruction.Value, maxValue - 2, maxValue);
-                        _a = (_a * z) % maxValue;
-                        _b = (_b * z) % maxValue;
+                        _a = (_a * instruction.Value) % maxValue;
+                        _b = (_b * instruction.Value) % maxValue;
                         break;
                 }
             }
         }
 
-        private long FindResultIndex(long maxValue, long index) {
-            foreach (var instruction in _instructions) {
-                switch (instruction.InstructionType) {
-                    case enumInstructionType.CutCards:
-                        index = (index - instruction.Value) % maxValue;
-                        if (index < 0) {
-                            index += maxValue;
-                        }
-                        break;
-                    case enumInstructionType.DealNewStack:
-                        index = maxValue - index - 1;
-                        break;
-                    case enumInstructionType.DealWithIncrement:
-                        index = (index * instruction.Value) % maxValue;
-                        break;
-                }
-            }
-            return index;
-        }
+        private long Answer2(BigInteger maxValue, BigInteger count) {
+            SolveAB(maxValue);
 
-        private int IndexOf(int num, int[] cards) {
-            for (int index = 0; index < cards.Length; index++) {
-                if (cards[index] == num) {
-                    return index;
-                }
-            }
-            return 0;
-        }
-
-        private int[] DealWithIncrement(int[] cards, int value) {
-            var newCards = new int[cards.Length];
-            int index = 0;
-            int newIndex = 0;
-            for (int count = 1; count <= cards.Length; count++) {
-                newCards[index] = cards[newIndex];
-                index = (index + value) % cards.Length;
-                newIndex++;
-            }
-            return newCards;
-        }
-
-        private int[] DealNewStack(int[] cards) {
-            var newCards = new int[cards.Length];
-            for (int index = 0; index < cards.Length; index++) {
-                newCards[index] = cards[cards.Length - index - 1];
-            }
-            return newCards;
-        }
-
-        private int[] CutCards(int[] cards, int value) {
-            var newCards = new int[cards.Length];
-            int count = 0;
-            int start = value;
-            if (value < 0) {
-                start = cards.Length + value;
-            }
-            for (int index = start; index < cards.Length; index++) {
-                newCards[count] = cards[index];
-                count++;
-            }
-            for (int index = 0; index < start; index++) {
-                newCards[count] = cards[index];
-                count++;
-            }
-            return newCards;
+            // I have no idea how this works. But it was the last of all AoC problems, so sue me
+            var r = (_b * Power.Exp(1 - _a, maxValue - 2, maxValue)) % maxValue;
+            r = ((2020 - r) * Power.Exp(_a, count * (maxValue - 2), maxValue) + r) % maxValue;
+            return (long)r;
         }
 
         private void GetInstructions(List<string> input) {
