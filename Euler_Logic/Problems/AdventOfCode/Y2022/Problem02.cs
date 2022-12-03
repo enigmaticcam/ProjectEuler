3828d680058d7eff0cb5e02cae013f4adade3c92
Cameron Tangen <ctangen81@gmail.com>
2022-12-03 09:33:12 -0800

    Solved AOC 2022 problems 2 and 3.

---
+++ Euler_Logic/Problems/AdventOfCode/Y2022/Problem02.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler_Logic.Problems.AdventOfCode.Y2022 {
+    public class Problem02 : AdventOfCodeBase {
+        public override string ProblemName => "Advent of Code 2022: 2";
+
+        public override string GetAnswer() {
+            return Answer1(Input()).ToString();
+        }
+
+        public override string GetAnswer2() {
+            return Answer2(Input()).ToString();
+        }
+
+        private int Answer1(List<string> input) {
+            var games = GetGames(input);
+            int total = 0;
+            foreach (var game in games) {
+                var score = GetScore(game);
+                total += score;
+            }
+            return total;
+        }
+
+        private int Answer2(List<string> input) {
+            var games = GetGames(input);
+            int total = 0;
+            foreach (var game in games) {
+                int second = 0;
+                if (game.Item2 == 1) {
+                    if (game.Item1 == 1) {
+                        second = 3;
+                    } else if (game.Item1 == 2) {
+                        second = 1;
+                    } else {
+                        second = 2;
+                    }
+                } else if (game.Item2 == 2) {
+                    second = game.Item1;
+                } else {
+                    if (game.Item1 == 1) {
+                        second = 2;
+                    } else if (game.Item1 == 2) {
+                        second = 3;
+                    } else {
+                        second = 1;
+                    }
+                }
+                var score = GetScore(new Tuple<int, int>(game.Item1, second));
+                total += score;
+            }
+            return total;
+        }
+
+        private int GetScore(Tuple<int, int> game) {
+            // 1 = rock
+            // 2 = paper
+            // 3 = scissors
+            if (game.Item1 == game.Item2)
+                return game.Item2 + 3;
+            if (game.Item2 == 1) {
+                if (game.Item1 == 2) {
+                    return game.Item2;
+                } else {
+                    return game.Item2 + 6;
+                }
+            } else if (game.Item2 == 2) {
+                if (game.Item1 == 1) {
+                    return game.Item2 + 6;
+                } else {
+                    return game.Item2;
+                }
+            } else {
+                if (game.Item1 == 1) {
+                    return game.Item2;
+                } else {
+                    return game.Item2 + 6;
+                }
+            }
+        }
+
+        private List<Tuple<int, int>> GetGames(List<string> input) {
+            return input.Select(x => {
+                var split = x.Split(' ');
+                int first = 0;
+                int second = 0;
+                switch (split[0]) {
+                    case "A":
+                        first = 1;
+                        break;
+                    case "B":
+                        first = 2;
+                        break;
+                    case "C":
+                        first = 3;
+                        break;
+                }
+                switch (split[1]) {
+                    case "X":
+                        second = 1;
+                        break;
+                    case "Y":
+                        second = 2;
+                        break;
+                    case "Z":
+                        second = 3;
+                        break;
+
+                }
+                return new Tuple<int, int>(first, second);
+            }).ToList();
+        }
+    }
+}
---
+++ Euler_Logic/Problems/AdventOfCode/Y2022/Problem03.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler_Logic.Problems.AdventOfCode.Y2022 {
+    public class Problem03 : AdventOfCodeBase {
+        public override string ProblemName => "Advent of Code 2022: 3";
+
+        public override string GetAnswer() {
+            return Answer1(Input()).ToString();
+        }
+
+        public override string GetAnswer2() {
+            return Answer2(Input()).ToString();
+        }
+
+        private int Answer1(List<string> input) {
+            var sacks = GetRucksacks(input);
+            SetShared(sacks);
+            return sacks
+                .Select(x => x.Priority)
+                .Sum();
+        }
+
+        private int Answer2(List<string> input) {
+            var sacks = GetRucksacks(input);
+            var groups = GetGroups(sacks);
+            SetShared(groups);
+            return groups
+                .Select(x => x.Priority)
+                .Sum();
+        }
+
+        private void SetShared(List<Group> groups) {
+            foreach (var group in groups) {
+                SetShared(group);
+            }
+        }
+
+        private void SetShared(Group group) {
+            var hash = new Dictionary<char, int>();
+            int powerOf2 = 1;
+            foreach (var set in group.Set) {
+                foreach (var digit in set) {
+                    if (!hash.ContainsKey(digit)) {
+                        if (powerOf2 == 1)
+                            hash.Add(digit, powerOf2);
+                    } else {
+                        hash[digit] |= powerOf2;
+                    }
+                }
+                powerOf2 *= 2;
+            }
+            group.Shared = hash.First(x => x.Value == 7).Key;
+            group.Priority = GetPriority(group.Shared);
+        }
+
+        private List<Group> GetGroups(List<Rucksack> sacks) {
+            var list = new List<Group>();
+            var group = new Group() { Set = new string[3] };
+            int count = 0;
+            foreach (var sack in sacks) {
+                group.Set[count] = sack.Full;
+                if (count == 2) {
+                    list.Add(group);
+                    group = new Group() { Set = new string[3] };
+                    count = 0;
+                } else {
+                    count++;
+                }
+            }
+            return list;
+        }
+
+        private void SetShared(List<Rucksack> sacks) {
+            int index = 0;
+            foreach (var sack in sacks) {
+                SetShared(sack, index);
+                index++;
+            }
+        }
+
+        private void SetShared(Rucksack sack, int index) {
+            var hash = new HashSet<char>(sack.Set1.ToArray());
+            foreach (var digit in sack.Set2) {
+                if (hash.Contains(digit)) {
+                    sack.Shared = digit;
+                    sack.Priority = GetPriority(sack.Shared);
+                    break;
+                }
+            }
+        }
+
+        private int GetPriority(char shared) {
+            var ascii = (int)shared;
+            if (ascii <= 90) {
+                return ascii - ((int)'Z' - 52);
+            } else {
+                return ascii - ((int)'z' - 26);
+            }
+        }
+
+        private List<Rucksack> GetRucksacks(List<string> input) {
+
+            return input.Select(x => {
+                var len = x.Length / 2;
+                var set1 = x.Substring(0, len);
+                var set2 = x.Substring(len);
+                return new Rucksack() { Set1 = set1, Set2 = set2, Full = x };
+            }).ToList();
+        }
+
+        private class Group {
+            public string[] Set { get; set; }
+            public char Shared { get; set; }
+            public int Priority { get; set; }
+        }
+
+        private class Rucksack {
+            public string Set1 { get; set; }
+            public string Set2 { get; set; }
+            public string Full { get; set; }
+            public char Shared { get; set; }
+            public int Priority { get; set; }
+        }
+    }
+}
