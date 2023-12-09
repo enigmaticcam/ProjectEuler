780e89cd2e8bbb5704a7845dfbe6d56e223492bb
Cameron Tangen <enigmaticcam@hotmail.com>
2023-12-08 21:27:29 -0800

    Solved AoC year 2023 days 8 and 9.

---
+++ Euler_Logic/Problems/AdventOfCode/Y2023/Problem08.cs
@@ -0,0 +1,118 @@
+﻿using Euler_Logic.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler_Logic.Problems.AdventOfCode.Y2023
+{
+    public class Problem08 : AdventOfCodeBase
+    {
+        private Dictionary<string, Node> _nodes;
+        private List<int> _directions;
+        public override string ProblemName => "Advent of Code 2023: 8";
+
+        public override string GetAnswer()
+        {
+            return Answer1(Input()).ToString();
+        }
+
+        public override string GetAnswer2()
+        {
+            return Answer2(Input()).ToString();
+        }
+
+        private int Answer1(List<string> input)
+        {
+            SetNodes(input);
+            SetDirections(input);
+            return FollowSteps();
+        }
+
+        private ulong Answer2(List<string> input)
+        {
+            SetNodes(input);
+            SetDirections(input);
+            return FollowStepsMultiple();
+        }
+        private ulong FollowStepsMultiple()
+        {
+            var current = _nodes
+                .Select(x => x.Value)
+                .Where(x => x.Name.Last() == 'A')
+                .ToList();
+            ulong final = 1;
+            foreach (var node in current)
+            {
+                ulong steps = (ulong)FollowStepsIndefinitely(node);
+                final = LCM.GetLCM(final, steps);
+            }
+            return final;
+        }
+
+        private int FollowStepsIndefinitely(Node current)
+        {
+            int directionIndex = 0;
+            int steps = 0;
+            do
+            {
+                steps++;
+                current = _nodes[current.Next[_directions[directionIndex]]];
+                if (current.Name.Last() == 'Z')
+                {
+                    return steps;
+                }
+                directionIndex = (directionIndex + 1) % _directions.Count;
+            } while (true);
+        }
+
+        private int FollowSteps()
+        {
+            var current = _nodes["AAA"];
+            int directionIndex = 0;
+            int steps = 0;
+            do
+            {
+                steps++;
+                current = _nodes[current.Next[_directions[directionIndex]]];
+                if (current.Name == "ZZZ")
+                    return steps;
+                directionIndex = (directionIndex + 1) % _directions.Count;
+            } while (true);
+        }
+
+        private void SetDirections(List<string> input)
+        {
+            _directions = new List<int>();
+            foreach (var digit in input[0])
+            {
+                if (digit == 'L')
+                {
+                    _directions.Add(0);
+                }
+                else
+                {
+                    _directions.Add(1);
+                }
+            }
+        }
+
+        private void SetNodes(List<string> input)
+        {
+            _nodes = new Dictionary<string, Node>();
+            for (int index = 2; index < input.Count; index++)
+            {
+                var line = input[index];
+                var node = new Node() { Next = new string[2] };
+                node.Name = line.Substring(0, 3);
+                node.Next[0] = line.Substring(7, 3);
+                node.Next[1] = line.Substring(12, 3);
+                _nodes.Add(node.Name, node);
+            }
+        }
+
+        private class Node
+        {
+            public string Name { get; set; }
+            public string[] Next { get; set; }
+        }
+    }
+}
---
+++ Euler_Logic/Problems/AdventOfCode/Y2023/Problem09.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler_Logic.Problems.AdventOfCode.Y2023
+{
+    public class Problem09 : AdventOfCodeBase
+    {
+        public override string ProblemName => "Advent of Code 2023: 9";
+
+        public override string GetAnswer()
+        {
+            return Answer1(Input()).ToString();
+        }
+
+        public override string GetAnswer2()
+        {
+            return Answer2(Input()).ToString();
+        }
+
+        private long Answer1(List<string> input)
+        {
+            return CalcSumAll(input, false);
+        }
+
+        private long Answer2(List<string> input)
+        {
+            return CalcSumAll(input, true);
+        }
+
+        private long CalcSumAll(List<string> input, bool reverse)
+        {
+            long total = 0;
+            foreach (var line in input)
+            {
+                var split = line.Split(' ')
+                    .Select(x => Convert.ToInt64(x))
+                    .ToList();
+                if (reverse)
+                    split.Reverse();
+                var next = CalcNext(split);
+                total += next;
+            }
+            return total;
+        }
+
+        private long CalcNext(List<long> nums)
+        {
+            if (nums.Count == 0)
+                return 0;
+            var next = new List<long>();
+            for (int index = 1; index < nums.Count; index++)
+            {
+                next.Add(nums[index] - nums[index - 1]);
+            }
+            return nums.Last() + CalcNext(next);
+        }
+    }
+}
