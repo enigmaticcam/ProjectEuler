0b4c15042d129ffeef146b0166b963369d6bdb3d
Cameron Tangen <ctangen81@gmail.com>
2024-02-01 15:00:39 -0800

    Solved AoC year 2023 day 25.

--- Euler_Logic/Problems/AdventOfCode/Y2023/Problem25.cs
+++ Euler_Logic/Problems/AdventOfCode/Y2023/Problem25.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Euler_Logic.Problems.AdventOfCode.Y2023;
@@ -8,75 +8,102 @@ public class Problem25 : AdventOfCodeBase {
     public override string ProblemName => "Advent of Code 2023: 25";
 
     public override string GetAnswer() {
-        //return Answer1(Input_Test(1)).ToString();
         return Answer1(Input()).ToString();
-    }
-
-    public override string GetAnswer2() {
-        return Answer2(Input()).ToString();
     }
 
     private int Answer1(List<string> input) {
         var connections = GetConnections(input);
-        var shorts = GetShortConnects(connections);
-        //Save(shorts);
         Complete(connections);
-        return Solve(connections, shorts);
+        return Solve(connections);
     }
 
-    private void Save(List<string[]> shorts) {
-        using (var stream = new StreamWriter(@"c:\temp\temp.txt")) {
-            foreach (var sho in shorts) {
-                stream.WriteLine($"{sho[0]} {sho[1]}");
-            }
+    private int Solve(Dictionary<string, Connection> connections) {
+        FindPathCommon(connections);
+        RemoveCommon(connections);
+        return CountGroups(connections);
+    }
+
+    private void RemoveCommon(Dictionary<string, Connection> connections) {
+        var toRemove = _counts.OrderByDescending(x => x.Value).Take(3);
+        foreach (var remove in toRemove) {
+            var split = remove.Key.Split(',');
+            connections[split[0]].End.Remove(split[1]);
+            connections[split[1]].End.Remove(split[0]);
         }
     }
 
-    private string Answer2(List<string> input) {
-        return "";
-    }
-
-    private int Solve(Dictionary<string, Connection> connections, List<string[]> shorts) {
-        for (int index1 = 0; index1 < shorts.Count; index1++) {
-            RemoveShort(connections, shorts[index1]);
-            for (int index2 = index1 + 1; index2 < shorts.Count; index2++) {
-                RemoveShort(connections, shorts[index2]);
-                CountGroups(connections);
-                AddShort(connections, shorts[index2]);
-            }
-            AddShort(connections, shorts[index1]);
+    private void FindPathCommon(Dictionary<string, Connection> connections) {
+        var random = new Random();
+        for (int count = 1; count <= 200; count++) {
+            var first = connections.Keys.ElementAt(random.Next(connections.Count));
+            string second;
+            do {
+                second = connections.Keys.ElementAt(random.Next(connections.Count));
+            } while (second == first);
+            FindPath(connections, first, second);
         }
-        CountGroups(connections);
-        return 0;
     }
 
-    private void RemoveShort(Dictionary<string, Connection> connections, string[] shor) {
-        connections[shor[0]].End.Remove(shor[1]);
-        connections[shor[1]].End.Remove(shor[0]);
+    private Dictionary<string, int> _counts = new();
+    private void FindPath(Dictionary<string, Connection> connections, string start, string end) {
+        var list = new LinkedList<string>();
+        var hash = new HashSet<string>();
+        list.AddLast(start);
+        hash.Add(start);
+        connections[start].From = null;
+        do {
+            var current = connections[list.First.Value];
+            if (current.Start == end)
+                break;
+            foreach (var next in connections[current.Start].End) {
+                if (!hash.Contains(next)) {
+                    hash.Add(next);
+                    list.AddLast(next);
+                    connections[next].From = current;
+                }
+            }
+            list.RemoveFirst();
+        } while (true);
+        var path = end;
+        do {
+            string node1 = path;
+            path = connections[path].From.Start;
+            string node2 = path;
+            if (path != start)
+                AddToCount(node1, node2);
+        } while (path != start);
     }
 
-    private void AddShort(Dictionary<string, Connection> connections, string[] shor) {
-        connections[shor[0]].End.Add(shor[1]);
-        connections[shor[1]].End.Add(shor[0]);
+    private void AddToCount(string node1, string node2) {
+        string first = node1;
+        string second = node2;
+        if (string.Compare(first, second) > 0) {
+            first = node2;
+            second = node1;
+        }
+        var key = $"{first},{second}";
+        if (!_counts.ContainsKey(key)) {
+            _counts.Add(key, 1);
+        } else {
+            _counts[key]++;
+        }
     }
 
-    private int[] _counts = new int[2];
-    private bool CountGroups(Dictionary<string, Connection> connections) {
-        _counts[0] = 0;
-        _counts[1] = 0;
+    private int CountGroups(Dictionary<string, Connection> connections) {
+        var counts = new int[2];
+        counts[0] = 0;
+        counts[1] = 0;
         int index = 0;
-        bool isGood = false;
         var hash = new HashSet<string>();
         foreach (var connection in connections.Values) {
             if (!hash.Contains(connection.Start)) {
-                _counts[index] = BFS(connections, hash, connection.Start);
+                counts[index] = BFS(connections, hash, connection.Start);
                 if (hash.Count == connections.Count)
                     break;
                 index++;
-                isGood = true;
             }
         }
-        return isGood;
+        return counts[0] * counts[1];
     }
 
     private int BFS(Dictionary<string, Connection> connections, HashSet<string> hash, string start) {
@@ -111,16 +138,6 @@ public class Problem25 : AdventOfCodeBase {
         }
     }
 
-    private List<string[]> GetShortConnects(Dictionary<string, Connection> connections) {
-        var list = new List<string[]>();
-        foreach (var connect in connections) {
-            foreach (var end in connect.Value.End) {
-                list.Add(new string[2] { connect.Value.Start, end });
-            }
-        }
-        return list;
-    }
-
     private Dictionary<string, Connection> GetConnections(List<string> input) {
         var hash = new Dictionary<string, Connection>();
         foreach (var line in input) {
@@ -140,5 +157,6 @@ public class Problem25 : AdventOfCodeBase {
     private class Connection {
         public string Start { get; set; }
         public HashSet<string> End { get; set; }
+        public Connection From { get; set; }
     }
 }
